39c5c29a560f5d7fc731c852919aac11527a78fc
Siwei Zhuang <siwei.zhuang@nicta.com.au>
2016-08-01 11:23:41 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+camkes-usb-tests
+===============
+
+Tests for the USB driver in CAmkES environment.
+
+This repository also contains the repo manifest for the tests.
+
+To fetch do:
+
+    mkdir camkes-usb-tests
+    cd camkes-usb-tests
+    repo init -u https://github.com/seL4-projects/camkes-usb-tests.git
+    repo sync
+
---
+++ apps/storage/components/Driver/src/drv.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2015, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#include <camkes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int run() {
+    printf("Starting the driver\n");
+    printf("-------------------\n");
+
+    printf("After the driver\n");
+    return 0;
+}
