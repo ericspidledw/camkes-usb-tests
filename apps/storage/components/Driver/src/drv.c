700c2f8db82cd7852cbb4e6633d8e600fb9dbdde
Siwei Zhuang <siwei.zhuang@data61.csiro.au>
2016-10-12 17:46:56 +1100

    Configure to run on x86.

--- apps/storage/components/Driver/src/drv.c
+++ apps/storage/components/Driver/src/drv.c
@@ -12,9 +12,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <usb/usb.h>
+
+static void usb_irq_handler(void *arg)
+{
+	usb_t *priv = (usb_t*)arg;
+
+	usb_handle_irq(priv);
+
+	irq_reg_callback(usb_irq_handler, priv);
+}
+
 int run() {
+    int err;
+    usb_t *usb;
+    usb_dev_t usb_storage;
+    ps_io_ops_t io_ops;
+
     printf("Starting the driver\n");
     printf("-------------------\n");
+
+    camkes_io_ops(&io_ops);
+    usb = malloc(sizeof(usb_t));
+
+    err = usb_init(USB_HOST2, &io_ops, usb);
+    assert(err);
+
+    irq_reg_callback(usb_irq_handler, usb);
+
+    while (1) {
+	    usb_storage = usb_get_device(usb, 5);
+	    if (usb_storage) {
+		    break;
+	    }
+    }
+
+    usb_lsusb(usb, 1);
+    usb_storage_bind(usb_storage, NULL);
 
     printf("After the driver\n");
     return 0;
